9a2d41a70ceada5b12875782811cd8c9cfd4f575
fjmunozp <fernando.munoz.p@utec.edu.pe>
2024-11-19 14:29:28 -0500

    hola

---
+++ src/main/java/com/example/s14retoutec/S14RetoUtecApplication.java
@@ -0,0 +1,13 @@
+package com.example.s14retoutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S14RetoUtecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S14RetoUtecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/s14retoutec/S14RetoUtecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s14retoutec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S14RetoUtecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
